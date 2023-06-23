4053e4f04e7d5b7a3e5b6e39edd0147c66cbed6c
Ammon  Shepherd <ammon.uva@gmail.com>
2023-06-23 15:43:03 -0400

    Add button and button function
    Add the python modules for reading a physical button push and a new
    function for when the button is pushed. The new function will start the
    process for taking a picture and all the other fun stuff.

--- simplebooth.py
+++ simplebooth.py
@@ -1,5 +1,22 @@
+import time
+
 import tkinter as tk
 from PIL import Image, ImageDraw, ImageFont, ImageTk
+
+from gpiozero import LED, Button
+
+######################################################################
+#
+#         CONFIG SETTINGS
+#         settings that users can change
+#######################################################################
+# pinout https://pinout.xyz
+# purple wire (switch) to physical/board pin 7
+# black wire (both ground) to physical/board pin 9
+# blue wire (led) to physical/board pin 11
+BUTTON_PIN = 4
+LED_PIN = 17
+##################### END CONFIG SETTINGS ############################
 
 
 ######################################################################
@@ -13,29 +30,81 @@ screen_width = win.winfo_screenwidth()
 win.grid_columnconfigure((0, 2), weight=1)
 win.configure(bg="black")
 
-# show idle instructions
-instructions = tk.StringVar()
-instruct_label = tk.Label(win, textvariable=instructions, font=(
+# Create the instructions variable and label
+instructions_text = tk.StringVar()
+instructions_label = tk.Label(win, textvariable=instructions_text, font=(
     "Arial", 85), wraplength=screen_width, bg="black", fg="white")
-
+# Create the logo label
 booth_icon = Image.open("simplebooth-icon.png")
 pic = ImageTk.PhotoImage(booth_icon)
-icon_label = tk.Label(win, image=pic, bg="black")
-icon_label.image = pic
+logo_label = tk.Label(win, image=pic, bg="black")
+logo_label.image = pic
 ##################### END GUI SETTINGS ############################
 
 
+######################################################################
+#
+#        HARDWARE SETUP
+#
+#######################################################################
+# set up button and LED objects
+blue_button = Button(BUTTON_PIN)
+blue_led = LED(LED_PIN)
+##################### END HARDWARE SETUP ############################
+
+
+######################################################################
+#
+#        Functions
+#
+#######################################################################
+
 def main_screen():
-    global icon_label
-    global instructions
-    global instruct_label
+    """Show the main screen to get users to push the button."""
+    global logo_label
+    global instructions_text
+    global instructions_label
 
-    icon_label.grid(row=0, column=1, sticky="ew")
+    # Blink the button LED
+    blue_led.blink()
 
-    instructions.set("Press the button to take pictures!")
-    instruct_label.grid(row=1, column=1, sticky="ew")
+    # Show the logo and text
+    logo_label.grid(row=0, column=1, sticky="ew")
+    instructions_text.set("Press the button to take pictures!")
+    instructions_label.grid(row=1, column=1, sticky="ew")
+
+    # Wait for the button to be pressed, then fire of the function
+    blue_button.when_pressed = button_pressed
 
 
+def button_pressed():
+    """After button press, take the pictures, print pictures, and if 
+    available, upload pictures to Google Doc and show a QR Code. Then
+    show the main screen."""
+    global logo_label
+    global instructions_label
+    global instructions_text
+
+    # Turn off the blinking
+    blue_led.off()
+
+    # Set new instructions_text, and pause for 6 seconds
+    instructions_text.set("Get ready to take 3 pictures.")
+    time.sleep(6)
+
+    # Remove the logo and the instructions_text
+    logo_label.grid_remove()
+    instructions_label.grid_remove()
+
+    # And back to the main screen
+    main_screen()
+
+
+######################################################################
+#
+#        Main Script Functionality
+#
+#######################################################################
 # Call the main screen function to get things going!
 main_screen()
 
@@ -44,8 +113,6 @@ main_screen()
 def close_window(e):
     win.destroy()
     exit()
-
-
 win.bind('<Escape>', lambda e: close_window(e))
 
 win.mainloop()
